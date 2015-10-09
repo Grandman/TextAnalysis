46588019b2037cda9815e2442b2607a24d9a1990
Grandman73 <root@grandman73.ru>
2015-10-10 00:39:21 +0300

    text statistic

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TextAnalysis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var str =
+            "Ехал древний Грека через реку, видит древний Грека в реке древний рак. Сунул древний Грека руку в реку. Древний рак за руку древнего Греку цап";
+            var dictionary = StatisticCalculation.CountFrequencyWords(
+                "Ехал Грека через реку, видит Грека в реке рак. Сунул Грека руку в реку. Рак за руку Греку цап​");
+            dictionary.ToList().OrderBy(item => item.Value).Reverse().ToList().ForEach(x => textBox1.AppendText(x.Key + " : " + x.Value + Environment.NewLine));
+            textBox1.AppendText(StatisticCalculation.MutualInformation(str, "древний грека").ToString() + Environment.NewLine);
+            textBox1.AppendText(StatisticCalculation.TScore(str, "древний грека").ToString());
+        }
+    }
+}
---
+++ StatisticCalculation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TextAnalysis
+{
+    class StatisticCalculation
+    {
+        private static readonly char[] DelimiterChars = { ' ', ',', '.', ':', '\t' };   
+
+        public static Dictionary<string, int> CountFrequencyWords(string text)
+        {
+            Dictionary<string,int> frequencyDictionary = new Dictionary<string, int>();
+            string[] words = text.Split(DelimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var downCaseWord in words.Select(word => word.ToLower()))
+            {
+                if (frequencyDictionary.ContainsKey(downCaseWord))
+                {
+                    frequencyDictionary[downCaseWord]++;
+                    continue;
+                }
+                frequencyDictionary.Add(downCaseWord, 1);
+            }
+            return frequencyDictionary;
+        }
+
+        public static double MutualInformation(string text, string phrase)
+        {
+            phrase = phrase.ToLower();
+            text = text.ToLower();
+            string textWithoutPhrase = text.Replace(phrase, "");
+            var wordsWithPhrase = text.Split(DelimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            var wordsWithoutPhrase= textWithoutPhrase.Split(DelimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            var phraseFrequency = (wordsWithPhrase.Length - wordsWithoutPhrase.Length) / 2;
+            var freqDictionary = CountFrequencyWords(text);
+            var words = phrase.Split(' ');
+            int frequenceOfFirstWord = freqDictionary.FirstOrDefault(x => x.Key.Contains(words[0])).Value;
+            int frequenceOfSecondWord = freqDictionary.FirstOrDefault(x => x.Key.Contains(words[1])).Value;
+
+            return Math.Log((phraseFrequency * wordsWithPhrase.Length) / (frequenceOfFirstWord * frequenceOfSecondWord), 2);
+        }
+
+        public static double TScore(string text, string phrase)
+        {
+            phrase = phrase.ToLower();
+            text = text.ToLower();
+            string textWithoutPhrase = text.Replace(phrase, "");
+            var wordsWithPhrase = text.Split(DelimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            var wordsWithoutPhrase = textWithoutPhrase.Split(DelimiterChars, StringSplitOptions.RemoveEmptyEntries);
+            var phraseFrequency = (wordsWithPhrase.Length - wordsWithoutPhrase.Length) / 2;
+            var freqDictionary = CountFrequencyWords(text);
+            var words = phrase.Split(' ');
+            int frequenceOfFirstWord = freqDictionary.FirstOrDefault(x => x.Key.Contains(words[0])).Value;
+            int frequenceOfSecondWord = freqDictionary.FirstOrDefault(x => x.Key.Contains(words[1])).Value;
+            int phraseOccurrencesCountInText = Regex.Matches(text, phrase).Count;
+            return (phraseFrequency - (frequenceOfFirstWord*frequenceOfSecondWord / phraseOccurrencesCountInText)) /
+                   phraseFrequency;
+        }
+
+    }
+}
